2997c2af7aafdc511bef18e4efd4f79abb14921d
hitokun-s <jgpuauno@gmail.com>
2015-11-29 13:24:57 +0100

    temp

---
+++ test.js
@@ -0,0 +1,29 @@
+console.log("hitokun!");
+
+//var fs = require("fs");
+//var request = require("request");
+//var option = {
+//    url: "https://vision.eyeem.com/photohackday/photos",
+//    headers: {
+//        "Authorization": "PHOTOHACKDAY123"
+//    }
+//}
+//var path = "public/img/58331258.jpg";
+//fs.createReadStream(path).pipe(request.put(option, function (error, res) {
+//    console.log(res);
+//    console.log(error);
+//}));
+
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+var commandLine = "pwd";
+child = exec(commandLine, function (error, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    //sys.print('stdout: ' + stdout);
+    //sys.print('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
